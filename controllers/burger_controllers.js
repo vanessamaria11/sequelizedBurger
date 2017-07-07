14403262227df4791f09d121594df93338d1c62a
vanessamaria11 <vanessamariaotto@gmail.com>
2017-07-07 01:49:52 -0400

    working hw

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,49 +1,43 @@
-var express = require ("express");
-
+var express = require("express");
 var router = express.Router();
 
-var burger = require("../models/burger.js");
+var db = require("../models");
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  cat.all(function(data) {
-    var hbsObject = {
-      cats: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get("/", function(req, res){
+	res.redirect("/burgers");
 });
 
-router.post("/", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function() {
-    res.redirect("/");
-  });
+router.get("/burgers", function(req, res){
+	db.BurgerPost.findAll()
+	.then(function(burgerInfo){
+		console.log(burgerInfo);
+		var handlebars = { burger: burgerInfo};
+		return res.render("index", handlebars);
+	});
 });
 
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function() {
-    res.redirect("/");
-  });
+router.post("/burgers/create", function(req, res){
+	db.BurgerPost.create({
+		burger_name: req.body.burger_name
+	})
+	.then(function(burgerInfo){
+		console.log(burgerInfo);
+		res.redirect("/");
+	});
 });
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function() {
-    res.redirect("/");
-  });
+router.put("/burgers/update", function(req, res){
+	db.BurgerPost.update({
+		devoured: true
+	},
+	{
+		where: {
+			id: req.body.burger_id
+		}
+	}
+	).then(function(burgerInfo){
+		res.redirect("/");
+	});
 });
 
-// Export routes for server.js to use.
 module.exports = router;
--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burgersTable (burger_name, devoured)VALUES ('Turkey Burger', true);
-INSERT INTO burgersTable (burger_name, devoured)VALUES ('Chicken Burger', true);
-INSERT INTO burgersTable (burger_name, devoured)VALUES ('Black Bean Burger', true);
-INSERT INTO burgersTable (burger_name, devoured)VALUES ('Double Cheeseburger', true);
